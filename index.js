0615d56d3c77f643dc36a6c89003f31d5d110fd0
zabuter <steven@fradestar.io>
2020-01-15 12:15:50 -0500

    first run fixes

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const installPackages = () => {
   return new Promise(resolve => {
     console.log("\nInstalling Dependencies\n".cyan);
     shell.exec(
-      `npm install -D typescript node-sass axios react-router react-router-dom lodash framer-motion reset-css moment @types/node @types/axios @types/lodash @types/react-router @types/react-router-dom @types/moment`,
+      `npm install -D typescript node-sass axios react-router react-router-dom lodash framer-motion reset-css moment @types/node @types/lodash @types/react-router @types/react-router-dom`,
       () => {
         console.log("\nFinished installing packages\n".green);
         resolve();
@@ -63,7 +63,7 @@ const updateTemplates = () => {
     let promises = [];
 
     //remove files
-    Object.keys([`App.js`, `App.css`, `logo.svg`]).forEach((fileName, i) => {
+    [`App.js`, `App.css`, `logo.svg`].forEach((fileName, i) => {
       promises.push(
         new Promise(res => {
           fs.unlink(`${appDirectory}/src/${fileName}`, err => {
@@ -76,15 +76,12 @@ const updateTemplates = () => {
 
     //add files
     promises.push(
-      fs.copy(
-        `${process.cwd()}/templates`,
-        `${appDirectory}/src/`,
-        {},
-        function(err) {
-          if (err) return console.log(err);
-          res();
-        }
-      )
+      fs.copy(`${__dirname}/templates`, `${appDirectory}/src/`, {}, function(
+        err
+      ) {
+        if (err) return console.log(err);
+        res();
+      })
     );
 
     Promise.all(promises).then(resolve);
